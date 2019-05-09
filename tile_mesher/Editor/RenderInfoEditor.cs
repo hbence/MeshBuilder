7318c9822d6346068b4422b002a51fdd25fc38b9
Halmosi Bence <hbence88@gmail.com>
2019-05-09 17:31:57 +0200

    - added RenderInfoEditor

---
+++ tile_mesher/Editor/RenderInfoEditor.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace MeshBuilder
+{
+    [CustomPropertyDrawer(typeof(MeshBuilderDrawer.RenderInfo))]
+    public class RenderInfoEditor : PropertyDrawer
+    {
+        private const bool ExpandChildren = true;
+
+        private bool groupFoldout = false;
+        private float height = EditorGUIUtility.singleLineHeight;
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return height;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            float lineHeight = EditorGUIUtility.singleLineHeight + 2;
+            float start = position.y;
+            var lineRect = new Rect(position.x, position.y, position.width, lineHeight);
+
+            lineRect = EditorGUI.IndentedRect(lineRect);
+
+            groupFoldout = EditorGUI.BeginFoldoutHeaderGroup(lineRect, groupFoldout, property.displayName);
+
+            if (groupFoldout)
+            {
+                EditorGUI.indentLevel++;
+
+                lineRect.y += lineHeight;
+                EditorGUI.PropertyField(lineRect, property.FindPropertyRelative("hide"));
+
+                var subMeshInfo = property.FindPropertyRelative("subMeshInfo");
+
+                lineRect.y += lineHeight;
+                GUI.Label(lineRect, "Submeshes: " + subMeshInfo.arraySize);
+
+                DisplayArray(subMeshInfo, ref lineRect);
+
+                lineRect.y += lineHeight;
+                bool add = GUI.Button(lineRect, "Add Submesh");
+
+                if (add)
+                {
+                    ++subMeshInfo.arraySize;
+                }
+
+                EditorGUI.indentLevel--;
+            }
+
+            lineRect.y += lineHeight;
+            height = lineRect.y - start;
+
+            EditorGUI.EndFoldoutHeaderGroup();
+            EditorGUI.EndProperty();
+        }
+
+        private void DisplayArray(SerializedProperty arrayProp, ref Rect lineRect)
+        {
+            for (int i = 0; i < arrayProp.arraySize; ++i)
+            {
+                bool remove = false;
+                var child = arrayProp.GetArrayElementAtIndex(i);
+                DisplaySubmesh(child, i, out remove, ref lineRect);
+
+                if (remove)
+                {
+                    arrayProp.DeleteArrayElementAtIndex(i);
+                    break;
+                }
+            }
+        }
+
+        private void DisplaySubmesh(SerializedProperty prop, int count, out bool remove, ref Rect lineRect)
+        {
+            float lineHeight = EditorGUIUtility.singleLineHeight + 2;
+
+            remove = false;
+
+            prop.NextVisible(ExpandChildren);
+            var material = prop.Copy();
+            prop.NextVisible(ExpandChildren);
+            var recieveShadow = prop.Copy();
+            prop.NextVisible(ExpandChildren);
+            var castShadow = prop;
+
+            var bgRect = lineRect;
+            bgRect.y += lineHeight - 4;
+            bgRect.height += lineHeight * 3.25f;
+            EditorGUI.DrawRect(bgRect, new Color(1, 1, 1, 0.2f));
+
+            // name and remove button
+            lineRect.y += lineHeight;
+            var halfRect = lineRect;
+            halfRect.width = lineRect.width * 0.75f;
+            EditorGUI.LabelField(lineRect, "Submesh " + count);
+
+            halfRect.x += halfRect.width;
+            halfRect.y -= 2;
+            halfRect.width = lineRect.width - halfRect.width;
+            if (GUI.Button(halfRect, "Remove"))
+            {
+                remove = true;
+            }
+
+            lineRect.y += lineHeight;
+            EditorGUI.PropertyField(lineRect, material);
+            lineRect.y += lineHeight;
+            EditorGUI.PropertyField(lineRect, recieveShadow);
+            lineRect.y += lineHeight;
+            EditorGUI.PropertyField(lineRect, castShadow);
+
+            lineRect.y += lineHeight * 0.5f;
+        }
+    }
+}
