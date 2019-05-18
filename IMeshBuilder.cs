4a517e9767b08843e08a5c82981d0cf11c7b4723
Halmosi Bence <hbence88@gmail.com>
2019-05-18 12:58:53 +0200

    - IMeshBuilder interface simplified

--- IMeshBuilder.cs
+++ IMeshBuilder.cs
@@ -4,9 +4,4 @@ using UnityEngine;
 public interface IMeshBuilder : System.IDisposable
 {
     Mesh Mesh { get; }
-
-    void StartGeneration();
-    void EndGeneration();
-
-    bool IsGenerating { get; }
 }
