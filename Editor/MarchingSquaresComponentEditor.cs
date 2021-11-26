99ff578b1b0d13140a463644908e28a3a2317201
hbence <hbence88@gmail.com>
2021-11-26 22:30:47 +0100

    - added marching squares component editor

---
+++ Editor/MarchingSquaresComponentEditor.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using static MeshBuilder.MarchingSquaresComponent;
+
+namespace MeshBuilder
+{
+    [CustomEditor(typeof(MarchingSquaresComponent))]
+    public class MarchingSquaresComponentEditor : Editor
+    {
+        private class PropName
+        {
+            public const string MeshFilter = "meshFilter";
+            public const string InitializationPolicy = "initializationPolicy";
+            public const string Initialization = "initialization";
+            public const string DataCreationPolicy = "dataCreationPolicy";
+            public const string DataCreationInfo = "dataCreationInfo";
+        }
+
+        private class Props
+        {
+            public SerializedProperty meshFilter;
+            public SerializedProperty initializationPolicy;
+            public SerializedProperty initialization;
+            public SerializedProperty dataCreationPolicy;
+            public SerializedProperty dataCreationInfo;
+
+            public void FindProps(SerializedObject serializedObject)
+            {
+                meshFilter = serializedObject.FindProperty(PropName.MeshFilter);
+                initializationPolicy = serializedObject.FindProperty(PropName.InitializationPolicy);
+                initialization = serializedObject.FindProperty(PropName.Initialization);
+                dataCreationPolicy = serializedObject.FindProperty(PropName.DataCreationPolicy);
+                dataCreationInfo = serializedObject.FindProperty(PropName.DataCreationInfo);
+            }
+        }
+
+        private MarchingSquaresComponent marchingSquares;
+        private Props props;
+        
+        private void OnEnable()
+        {
+            marchingSquares = (MarchingSquaresComponent)target;
+            props = new Props();
+            props.FindProps(serializedObject);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(props.meshFilter);
+            EditorGUILayout.PropertyField(props.initializationPolicy);
+            EditorGUILayout.PropertyField(props.initialization);
+            EditorGUILayout.PropertyField(props.dataCreationPolicy);
+
+            DataManagementPolicy dataPolicy = (DataManagementPolicy)props.dataCreationPolicy.enumValueIndex;
+            if (dataPolicy == DataManagementPolicy.CreateOwn)
+            {
+                EditorGUILayout.PropertyField(props.dataCreationInfo);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Editor/MarchingSquaresInitializationInfoDrawer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using static MeshBuilder.MarchingSquaresComponent.InitializationInfo;
+
+namespace MeshBuilder
+{
+    [CustomPropertyDrawer(typeof(MarchingSquaresComponent.InitializationInfo))]
+    public class MarchingSquaresInitializationInfoDrawer : PropertyDrawer
+    {
+        private class PropName
+        {
+            public const string Type = "type";
+            public const string CellSize = "cellSize";
+            public const string Height = "height";
+            public const string LerpToExactEdge = "lerpToExactEdge";
+            public const string TaperedTopOffset = "taperedTopOffset";
+            public const string TaperedBottomOffset = "taperedBottomOffset";
+            public const string SegmentedOffsets = "segmentedOffsets";
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel += 1;
+
+            var typeProp = property.FindPropertyRelative(PropName.Type);
+            Type type = (Type)typeProp.enumValueIndex;
+
+            EditorGUILayout.PropertyField(typeProp);
+            EditorGUILayout.PropertyField(property.FindPropertyRelative(PropName.CellSize));
+            EditorGUILayout.PropertyField(property.FindPropertyRelative(PropName.Height));
+            EditorGUILayout.PropertyField(property.FindPropertyRelative(PropName.LerpToExactEdge));
+
+            if (type == Type.TaperedSideOnly || type == Type.TaperedNoBottomFullCell)
+            {
+                EditorGUILayout.PropertyField(property.FindPropertyRelative(PropName.TaperedTopOffset));
+                EditorGUILayout.PropertyField(property.FindPropertyRelative(PropName.TaperedBottomOffset));
+            }
+            else if (type == Type.SegmentedSideOnly || type == Type.SegmentedNoBottomFullCell)
+            {
+                EditorGUILayout.PropertyField(property.FindPropertyRelative(PropName.SegmentedOffsets));
+            }
+
+            EditorGUI.indentLevel = indent;
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
